cfad382c67e3290848c440bbfa34c4a9cf62b87c
MayckO2301 <mayck.ortiz@unipaz.edu.co>
2024-11-20 14:47:39 -0500

    Ajuste de vista de registro en el formulario

---
+++ app/src/main/java/com/example/sildermn/Registro.kt
@@ -0,0 +1,10 @@
+package com.example.sildermn
+
+data class Registro(
+    val Nombre: String = "",
+    val Edad: String = "",
+    val Sexo: String = "",
+    val AñoNacimiento: String = "",
+    val Correo: String = "",
+    val FotoBase64: String = ""
+)
